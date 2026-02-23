82e0470ed081d25feb336610ea238c49d67574fb
Sriharivishnu Janakiraman <j.sriharivishnu@gmail.com>
2026-02-23 09:03:59 +0800

    Add infrastructure/terraform/main.tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -2,11 +2,10 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = "ai-personalization-cluster"
-  role_arn = "${aws_iam_role.eks_cluster_role.arn}"
-
-  vpc_config {
-    subnet_ids = ["subnet-12345", "subnet-67890"]
-  }
+module "eks_cluster" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "personalization-engine"
+  cluster_version = "1.24"
+  subnets         = var.subnets
+  vpc_id          = var.vpc_id
 }
